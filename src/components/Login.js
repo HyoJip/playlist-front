bb75a88b6492f5672c4db50e50b89d7a476163f3
jeeyani <jeeyeon27@naver.com>
2022-06-19 22:48:11 +0900

    로그인 페이지 메인으로

---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+
+//로그인 화면
+function Login() {
+    /*
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+ 
+	// input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+ 
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+ 
+	// 페이지 렌더링 후 가장 처음 호출되는 함수
+    useEffect(() => {
+        axios.get('/user_inform/login')
+        .then(res => console.log(res))
+        .catch()
+    },
+    // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
+    [])
+ 
+    */
+
+    // login 버튼 클릭 이벤트
+    const onClickLogin = () => {
+        console.log('click login')
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <div>
+                <label htmlFor='input_id'>ID : </label>
+                <input type='text' name='input_id' />
+            </div>
+            <div>
+                <label htmlFor='input_pw'>PW : </label>
+                <input type='password' name='input_pw'/>
+            </div>
+            <div>
+                <button type='button' onClick={onClickLogin}>Login</button>
+            </div>
+        </div>
+    )
+}
+ 
+export default Login;
