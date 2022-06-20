1037cebed64e45cd54d8b7f12cecd5b511c37911
HyoJip <mightling7@naver.com>
2022-06-20 20:59:21 +0900

    [Feat] health check test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 
-
-
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    return (
+        <div>
+        </div>
+    );
 }
 
 export default App;
